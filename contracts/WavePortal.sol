e167c89cb674b8d942f42a1122a1f3fafdb4e7d9
Rhaqim <anusiemj@gmail.com>
2022-02-27 11:07:39 +0100

    updating smart contract functiion to get all waves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,14 +6,30 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 public totalWaves;
+
+    event NewWave(address indexed from, string message, uint256 timestamp);
+
+    struct Wave {
+        address from;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave [] waves;
     
     constructor() {
         console.log("Yo yo, I am a contract and i'm alive!");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
